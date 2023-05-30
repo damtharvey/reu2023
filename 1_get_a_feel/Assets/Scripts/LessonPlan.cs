c52bdef3cf2cff8d4215f2f40869fc93f78caac4
Harvey Dam <35015208+damtharvey@users.noreply.github.com>
2023-05-30 12:56:21 -0600

    Correct lesson text

--- 1_get_a_feel/Assets/Scripts/LessonPlan.cs
+++ 1_get_a_feel/Assets/Scripts/LessonPlan.cs
@@ -81,7 +81,7 @@ public class LessonPlan : MonoBehaviour
                 + "Press A or D to decrease or increase weight.\n"
                 + "Press S or W to decrease or increase bias.";
         }
-        else if (state == State.Fuzzy && classifier.previousAccuracy >= 0.8)
+        else if (state == State.Fuzzy && classifier.previousAccuracy >= 0.85)
         {
             instructions.text =
                 "Find a line that best separates the red and blue dots. Bright dots are misclassified.\n"
@@ -90,6 +90,7 @@ public class LessonPlan : MonoBehaviour
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 state = State.Xor;
+                classifier.previousAccuracy = 0;
                 classifier.MakeXorInstance(100, -30, 30, 5);
                 instructions.text =
                     "Here is the (trivial in hindsight) XOR problem.\n"
@@ -98,12 +99,11 @@ public class LessonPlan : MonoBehaviour
             }
             
         }
-        else if (state == State.Fuzzy && classifier.previousAccuracy < 0.8)
+        else if (state == State.Fuzzy && classifier.previousAccuracy < 0.85)
         {
             instructions.text =
                 "Find a line that best separates the red and blue dots. Bright dots are misclassified.\n"
-                + "This instance has some noise. You probably can't classify them all correctly.\n"
-                + "Close enough. Press Space to continue.";
+                + "This instance has some noise. You probably can't classify them all correctly.";
         }
         // else // state == State.Xor
         // {
